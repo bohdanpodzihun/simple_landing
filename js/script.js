c82ac25e383f1054ce0a02ab956144b41d2fa2f6
bohdanpodzihun <bohdanpodzihun@gmail.com>
2023-03-06 20:09:44 +0200

    fix paths

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+alert("S")
+
 $(document).ready(function() {
     // плавна прокрутка меню
 
-    $('nav a[href^="#"]').click(function() {
+    $('.header a[href^="#"]').click(function() {
         let target = $(this).attr('href');
         $('html, body').animate({
             scrollTop: $(target).offset().top
         }, 500);
-        $('nav a[href^="#"]').parent().removeClass('active');
+        $('.header a[href^="#"]').parent().removeClass('active');
         $(this).parent().addClass('active');
         return false;
     }); 
